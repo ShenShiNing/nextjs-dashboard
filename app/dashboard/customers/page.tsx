8b4110954f28a07be8cc56456920122198d38287
Shen Shining <353824385@qq.com>
2025-03-31 21:02:05 +0800

    finished course

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Customers",
+};
+
 const Page = () => {
-    return <p>Customers Page</p>
-}
+  return <p>Customers Page</p>;
+};
 
 export default Page;
